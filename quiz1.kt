a75d09a229722bdff28eef9a568f3d8ea3f6f804
DP5-deviaanjani-019 <114506379+DP5-deviaanjani-019@users.noreply.github.com>
2024-10-26 22:16:14 +0700

    Update quiz1.kt 2

--- quiz1.kt
+++ quiz1.kt
@@ -15,3 +15,21 @@ class Quiz1 : AppCompatActivity() {
         "Apa fungsi dari 'Version Control System' (VCS)?",
         "Manakah dari berikut ini yang merupakan metode pengembangan perangkat lunak yang berfokus pada iterasi dan umpan balik?"
     )
+
+    private val options = listOf(
+        listOf("Application Programming Interface", "Automated Program Integration", "Application Process Information", "Automatic Programming Interface", "None of the above"),
+        listOf("Python", "Java", "JavaScript", "C++", "Ruby"),
+        listOf("Django", "React", "Spring", "Flutter", "Angular"),
+        listOf("Mengelola database", "Mengelola versi dan kolaborasi kode", "Mengoptimalkan kecepatan aplikasi", "Mengelola sistem operasi", "Meningkatkan keamanan aplikasi"),
+        listOf("Waterfall", "Agile", "V-Model", "Spiral", "Big Bang")
+    )
+
+    private val correctAnswers = listOf(
+        "Application Programming Interface",
+        "JavaScript",
+        "Flutter",
+        "Mengelola versi dan kolaborasi kode",
+        "Agile"
+    )
+
+    private var currentQuestionIndex = 0 
